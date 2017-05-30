2bc9e78e1dd34a440307811f0b0ebdefb663e280
kartuzovmax <kartuzovmax@hotmail.com>
2017-05-30 15:35:41 +0500

    show users on main view

--- src/js/controllers/main.controller.js
+++ src/js/controllers/main.controller.js
@@ -6,7 +6,9 @@ MainCtrl.$inject = ['$http'];
 function MainCtrl($http) {
   const vm = this;
 
-  $http.get('http://localhost:3000/api/users')
+  $http.get('http://localhost:3000/api/users', {
+    headers: {'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MSwiZW1haWwiOiJtYXhAbWF4LmNvbSIsImV4cCI6MTQ5NjIyNTk0N30.oCTzmAQwvTDfVeR1pCDBwJXWw6ttAPu47pfALhV0DHo'}
+  })
   .then(response => {
     vm.users = response.data;
     console.log(vm.users);
