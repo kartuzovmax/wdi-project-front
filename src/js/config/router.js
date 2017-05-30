4e5ef897a121f54c00e02f61ead26fdbb75f75e5
loyd0 <samloyd@hotmail.co.uk>
2017-05-30 15:39:09 +0100

    added files'

---
+++ src/js/config/router.js
@@ -0,0 +1,20 @@
+angular
+.moduler('Reddit')
+.config(Router);
+
+
+Router.$injection = ['$stateProvider', '$locationProvider', '$urlRouterProvider'];
+function Router($stateProvider, $locationProvider, $urlRouterProvider) {
+  $locationProvider.html5Mode(true);
+
+  $stateProvider
+  .state('home', {
+    url: '/',
+    templateUrl: '/js/views/home.html',
+    controller: 'MainCtrl',
+    controllerAs: 'main'
+  });
+
+  $urlRouterProvider.otherwise('/');
+
+}
